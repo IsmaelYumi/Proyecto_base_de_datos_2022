c84e81b0a41ca5ce48c1e65f99fb5ecd426972b1
IsmaelYumi <93888943+IsmaelYumi@users.noreply.github.com>
2023-01-05 01:06:22 -0500

    empezando

--- ProyectoBd/src/application/Main.java
+++ ProyectoBd/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,9 +12,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			FXMLLoader fmxlloader= new FXMLLoader();
+			 fmxlloader.setLocation(Main.class.getResource("VentanaPrincipal.fxml"));
+			Parent root = fmxlloader.load();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- ProyectoBd/src/module-info.java
+++ ProyectoBd/src/module-info.java
@@ -1,6 +1,7 @@
 module ProyectoBd {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
